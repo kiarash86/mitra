9e78477cc105be66d640b8a803137f7611836bd9
krsh <kiavash86habibi@gmail.com>
2026-08-25 08:57:07 +0330

    adding schema of organizations to migrations folder table -> id , name , slug , created_at , updated_at , deleted_at using uuid instead of serial for better security and using slug for places that we need to use them as url and a address to these organizations

---
+++ internal/db/migrations/001_create_organizations.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE organizations (
+    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name        VARCHAR(255) NOT NULL,
+    slug        VARCHAR(255) NOT NULL UNIQUE,
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at  TIMESTAMPTZ NOT NULL DEFAULT now(),
+    deleted_at  TIMESTAMPTZ
+);
