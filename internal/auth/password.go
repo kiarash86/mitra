06327cae05cf89fbcabcb760515b82908ab59343
krsh <kiavash86habibi@gmail.com>
2026-08-25 15:34:39 +0330

    adding func hashpassword to password in auth

--- internal/auth/password.go
+++ internal/auth/password.go
@@ -2,3 +2,13 @@ package auth
 
 import "golang.org/x/crypto/bcrypt"
 
+const bcryptCost = 12
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcryptCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
