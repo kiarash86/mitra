e68038aea99dd708050661bac8ab02b732dffc4e
krsh <kiavash86habibi@gmail.com>
2026-08-25 10:12:39 +0330

    adding func GetOrganizationMemberRole to organization_member query

--- internal/db/queries/organization_member.sql
+++ internal/db/queries/organization_member.sql
@@ -2,3 +2,8 @@
 INSERT INTO   organization_members (organization_id , user_id , role)
 VALUES ($1  , $2  , $3 )
 RETURNING *;
+
+-- name: GetOrganizationMemberRole :one
+SELECT role FROM organization_members 
+WHERE organization_id = $1 AND  user_id = $2;
+
