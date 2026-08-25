edf2558b529d9c039e43a9ee9edc7842e56d52a9
krsh <kiavash86habibi@gmail.com>
2026-08-25 10:18:18 +0330

    adding func ListTeamMembers to team_member query

--- internal/db/queries/team_member.sql
+++ internal/db/queries/team_member.sql
@@ -7,3 +7,11 @@ RETURNING *;
 SELECT role FROM team_members 
 WHERE team_id = $1 AND  user_id = $2;
 
+
+
+-- name: ListTeamMembers :many
+SELECT tm.*, u.email, u.full_name
+FROM team_members tm
+JOIN users u ON u.id = tm.user_id
+WHERE tm.team_id = $1;
+ 
