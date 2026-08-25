0ead5f38ab5f6e6293d4b60ebb2aee9aa3f7e90e
krsh <kiavash86habibi@gmail.com>
2026-08-25 10:17:58 +0330

    adding func GetTeamMemberRole to team_member query

--- internal/db/queries/team_member.sql
+++ internal/db/queries/team_member.sql
@@ -2,3 +2,8 @@
 INSERT INTO   team_members (team_id , user_id , role)
 VALUES ($1  , $2  , $3 )
 RETURNING *;
+
+-- name: GetTeamMemberRole :one
+SELECT role FROM team_members 
+WHERE team_id = $1 AND  user_id = $2;
+
