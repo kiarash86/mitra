ad18e54bc27a296366c333a8a732e45071eb936f
krsh <kiavash86habibi@gmail.com>
2026-08-25 10:32:57 +0330

    fixing a same typeo mistake in both users and teams. 'get' -> 'gen'

--- internal/db/migrations/002_create_users.up.sql
+++ internal/db/migrations/002_create_users.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE
     users (
-        id UUID PRIMARY KEY DEFAULT get_random_uuid (),
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
         full_name VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL UNIQUE,
         password_hash VARCHAR(255) NOT NULL,
--- internal/db/migrations/003_create_teams.up.sql
+++ internal/db/migrations/003_create_teams.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE
     teams (
-        id UUID PRIMARY KEY DEFAULT get_random_uuid (),
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
         name VARCHAR(255) NOT NULL,
         organization_id UUID NOT NULL REFERENCES organizations (id) ON DELETE CASCADE,
         created_at TIMESTAMPTZ NOT NULL DEFAULT now (),
