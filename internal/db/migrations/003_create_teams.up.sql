15b6519210b0509179b71989f9a35818574f5fc0
krsh <kiavash86habibi@gmail.com>
2026-08-25 09:11:19 +0330

    adding schema of teams to migrations folder table -> id , name , organization_id , created_at , updated_at , deleted_at

---
+++ internal/db/migrations/003_create_teams.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE
+    teams (
+        id UUID PRIMARY KEY DEFAULT get_random_uuid (),
+        name VARCHAR(255) NOT NULL,
+        organization_id UUID NOT NULL REFERENCES organizations (id) ON DELETE CASCADE,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT now (),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT now (),
+        deleted_at TIMESTAMPTZ
+    )
