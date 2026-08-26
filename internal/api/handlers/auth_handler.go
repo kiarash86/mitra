4fdd8f4f194a8f29e415169aad846997ab6301a1
krsh <kiavash86habibi@gmail.com>
2026-08-26 18:15:25 +0330

    feat : add struct for userResponse (id, fullname , email)

--- internal/api/handlers/auth_handler.go
+++ internal/api/handlers/auth_handler.go
@@ -5,37 +5,31 @@ import (
 	sqlc "github.com/kiarash86/mitra/internal/db/sqlc"
 )
 
-
 type AuthHandler struct {
-	queries * sqlc.Queries
-	tokens * auth.TokenManager
+	queries *sqlc.Queries
+	tokens  *auth.TokenManager
 }
-
 
 type registerRequest struct {
 	FullName string `json:"full_name" binding:"required,min=2,max=255"`
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=8"`
 }
- 
 
 type loginRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
 }
- 
 
-
-
-
-
-func NewAuthHandler(queries * sqlc.Queries , tokens * auth.TokenManager) *AuthHandler {
-	return  &AuthHandler{
-		queries: queries,
-		tokens: tokens,
-	}
+type userResponse struct {
+	ID       string `json:"id"`
+	FullName string `json:"full_name"`
+	Email    string `json:"email"`
 }
 
-
-
-
+func NewAuthHandler(queries *sqlc.Queries, tokens *auth.TokenManager) *AuthHandler {
+	return &AuthHandler{
+		queries: queries,
+		tokens:  tokens,
+	}
+}
