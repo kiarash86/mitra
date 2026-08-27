51e1861ce63ff69876ca4d404e1ca38f24299580
krsh <kiavash86habibi@gmail.com>
2026-08-27 23:07:37 +0330

    chore : defer pool.close

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,20 +8,21 @@ import (
 	"github.com/kiarash86/mitra/internal/config"
 )
 
-
 func main() {
 
-	cfg , err := config.Load()
+	cfg, err := config.Load()
 	if err != nil {
-		log.Fatalf("couldnt load config : %v" , err)
-	}	
-
-	ctx , cancel := context.WithCancel(context.Background())
-	defer cancel()
-	
-	pool , err := pgxpool.New(ctx , cfg.DatabaseURL)
-	if err != nil {
-		log.Fatalf("couldnt create db pool : %v" , err)
-		
+		log.Fatalf("couldnt load config : %v", err)
 	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	pool, err := pgxpool.New(ctx, cfg.DatabaseURL)
+	if err != nil {
+		log.Fatalf("couldnt create db pool : %v", err)
+
+	}
+	defer pool.Close()
+	
 }
