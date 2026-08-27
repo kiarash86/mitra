c4bd33622b1f3ae23e0299c70fbe8cc71581313c
krsh <kiavash86habibi@gmail.com>
2026-08-27 23:01:23 +0330

    feat : load config in main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	
+	"log"
+
+	"github.com/kiarash86/mitra/internal/config"
 )
 
 
 func main() {
-	
+
+	cfg , err := config.Load()
+	if err != nil {
+		log.Fatalf("couldnt load config : %v" , err)
+	}	
 }
