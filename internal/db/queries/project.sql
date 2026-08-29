19d4e3ea440394da780d9cbe255554f06bc62450
krsh <kiavash86habibi@gmail.com>
2026-08-29 21:05:13 +0330

    feat : add query 'UpdateProject' to queries in sqlc

--- internal/db/queries/project.sql
+++ internal/db/queries/project.sql
@@ -7,3 +7,10 @@ RETURNING *;
 -- name: GetProjectByID :one
 SELECT * FROM projects 
 WHERE id = $1;
+
+
+-- name: UpdateProject :one 
+UPDATE projects 
+SET name = $2 , description = $3 , updated_at = now()
+WHERE id = $1
+RETURNING *;
