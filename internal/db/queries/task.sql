a33439d632631407d3a7ceea996ba57b0f82a4b3
krsh <kiavash86habibi@gmail.com>
2026-08-29 21:28:22 +0330

    feat : add query 'GetTaskByID' to queries in sqlc

--- internal/db/queries/task.sql
+++ internal/db/queries/task.sql
@@ -2,3 +2,8 @@
 INSERT INTO tasks (project_id, title, description, priority, due_date, created_by)
 VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
+
+
+-- name: GetTaskByID :one
+SELECT * FROM task 
+WHERE id = $1;
