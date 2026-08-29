7e74c038a1af3cedba4bc136821e5046a99fef3d
krsh <kiavash86habibi@gmail.com>
2026-08-29 21:20:44 +0330

    feat : add query 'GetProjectMemberRole' to quries in sqlc

--- internal/db/queries/project_member.sql
+++ internal/db/queries/project_member.sql
@@ -2,3 +2,8 @@
 INSERT INTO project_members (project_id , user_id , role)
 WHERE ($1 , $2 , $3) 
 RETURNING *;
+
+
+-- name: GetProjectMemberRole :one 
+SELECT role FROM project_members 
+WHERE project_id = $1 AND user_id = $2;
