355d9f51f4cbee1022626766912499984cc3eb73
krsh <kiavash86habibi@gmail.com>
2026-08-29 23:10:01 +0330

    feat : add query 'ListCommentsByTask' to queries in sqlc

--- internal/db/queries/comment.sql
+++ internal/db/queries/comment.sql
@@ -7,3 +7,11 @@ RETURNING *;
 -- name: GetCommentByID :one
 SELECT * FROM comments
 WHERE id = $1;
+
+
+-- name: ListCommentsByTask :many
+SELECT c.* , u.full_name , u.email
+FROM comments c
+JOIN users u ON u.id = c.author_id
+WHERE c.task_id= $1
+ORDER BY c.created_at ASC;
