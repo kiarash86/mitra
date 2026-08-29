7004495b9f17b5e12dda96fcfc65306112e04380
krsh <kiavash86habibi@gmail.com>
2026-08-29 21:50:57 +0330

    feat : add 'AssignTaskToTeam' to queries in sqlc

--- internal/db/queries/task.sql
+++ internal/db/queries/task.sql
@@ -52,3 +52,10 @@ UPDATE tasks
 SET assigned_to_team_id = NULL, assigned_to_user_id = $2, updated_at = now()
 WHERE id = $1
 RETURNING *;
+
+
+-- name: AssignTaskToTeam :one
+UPDATE tasks
+SET assigned_to_team_id = $2, assigned_to_user_id = NULL, updated_at = now()
+WHERE id = $1
+RETURNING *;
