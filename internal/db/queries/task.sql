c789f288819a4c9a6ad60b4775ca318000ee7fae
krsh <kiavash86habibi@gmail.com>
2026-08-29 21:48:27 +0330

    feat : add query 'AssignTaskToUser' to queries in sqlc

--- internal/db/queries/task.sql
+++ internal/db/queries/task.sql
@@ -44,4 +44,10 @@ RETURNING *;
 -- name: SoftDeleteTask :exec
 UPDATE tasks
 SET deleted_at = now() 
-WHERE id= $1;I
+WHERE id= $1;
+
+
+-- name: AssignTaskToUser
+UPDATE tasks
+SET assigned_to_team_id = NULL, assigned_to_user_id = $2, updated_at = now()
+WHERE id = $1;
