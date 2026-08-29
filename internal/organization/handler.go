7e28d1034aa83cda038fcf9f82bf5e3b385a3d02
krsh <kiavash86habibi@gmail.com>
2026-08-30 00:46:40 +0330

    feat(handler) : add func NewHandler for constructor

--- internal/organization/handler.go
+++ internal/organization/handler.go
@@ -5,3 +5,9 @@ import "github.com/kiarash86/mitra/internal/db/sqlc"
 type Handler struct {
 	queries *sqlc.Queries
 }
+
+func NewHandler(queries *sqlc.Queries) *Handler {
+	return &Handler{
+		queries: queries,
+	}
+}
