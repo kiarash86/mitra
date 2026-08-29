c97e64bb723cf204b7e87abb58a61fa3e1e4b1b5
krsh <kiavash86habibi@gmail.com>
2026-08-30 00:00:39 +0330

    fix : typeo fix in task query in queries in db

--- internal/db/queries/task.sql
+++ internal/db/queries/task.sql
@@ -3,29 +3,24 @@ INSERT INTO tasks (project_id, title, description, priority, due_date, created_b
 VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
 
-
 -- name: GetTaskByID :one
 SELECT * FROM tasks
 WHERE id = $1;
 
-
 -- name: ListTasksByProject :many
 SELECT * FROM tasks
-WHERE project_id = $1 
+WHERE project_id = $1
 ORDER BY created_at DESC;
-
 
 -- name: ListTasksAssignedToUser :many
 SELECT * FROM tasks
-WHERE assigned_to_user_id = $1 
+WHERE assigned_to_user_id = $1
 ORDER BY due_date ASC NULLS LAST;
-
 
 -- name: ListTasksAssignedToTeam :many
 SELECT * FROM tasks
-WHERE assigned_to_team_id = $1 
+WHERE assigned_to_team_id = $1
 ORDER BY due_date ASC NULLS LAST;
-
 
 -- name: UpdateTask :one
 UPDATE tasks
@@ -33,26 +28,17 @@ SET title = $2, description = $3, priority = $4, due_date = $5, updated_at = now
 WHERE id = $1
 RETURNING *;
 
-
--- name:UpdateTaskStatus :one
+-- name: UpdateTaskStatus :one
 UPDATE tasks
-SET status = $2 , updated_at = now()
-WEHRE id= $1
-RETURNING *;
-
-
--- name: SoftDeleteTask :exec
-UPDATE tasks
-SET deleted_at = now() 
-WHERE id= $1;
-
-
--- name: AssignTaskToUser :one
-UPDATE tasks
-SET assigned_to_team_id = NULL, assigned_to_user_id = $2, updated_at = now()
+SET status = $2, updated_at = now()
 WHERE id = $1
 RETURNING *;
 
+-- name: AssignTaskToUser :one
+UPDATE tasks
+SET assigned_to_user_id = $2, assigned_to_team_id = NULL, updated_at = now()
+WHERE id = $1
+RETURNING *;
 
 -- name: AssignTaskToTeam :one
 UPDATE tasks
@@ -60,9 +46,13 @@ SET assigned_to_team_id = $2, assigned_to_user_id = NULL, updated_at = now()
 WHERE id = $1
 RETURNING *;
 
-
 -- name: UnassignTask :one
 UPDATE tasks
-SET assigned_to_team_id = NULL, assigned_to_user_id = NULL, updated_at = now()
+SET assigned_to_user_id = NULL, assigned_to_team_id = NULL, updated_at = now()
 WHERE id = $1
 RETURNING *;
+
+-- name: SoftDeleteTask :exec
+UPDATE tasks
+SET deleted_at = now()
+WHERE id = $1;
