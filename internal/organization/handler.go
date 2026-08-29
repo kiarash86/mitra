0c0769f4ee89117ab57615d11b0512609ae70369
krsh <kiavash86habibi@gmail.com>
2026-08-30 00:55:36 +0330

    chore(handler) : write func todos i need in handler

--- internal/organization/handler.go
+++ internal/organization/handler.go
@@ -1,8 +1,10 @@
 package organization
 
 import (
+	"context"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/kiarash86/mitra/internal/db/sqlc"
 )
 
@@ -35,4 +37,20 @@ type organizationMemberResponse struct {
 	Email     string    `json:"email"`
 	Role      string    `json:"role"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+func (h *Handler) Create(c *gin.Context) {
+	//TODO : CREATE NEW ORGANIZATION
+}
+
+func (h *Handler) GetBySlug(c *gin.Context) {
+	//TODO : GET BY BACKED NAME NOT FRONT ONE
+}
+
+func (h *Handler) ListMembers(c *gin.Context) {
+	//TODO : LISTING ALL EMPLOYEES
+}
+
+func (h *Handler) RemoveMember(c *gin.Context) {
+	//TODO : DELETE MEMBERS OF ORGANIZATION
 }
