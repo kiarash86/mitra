69677dfaf2d180f8a22df5741d71757c9b80390f
krsh <kiavash86habibi@gmail.com>
2026-08-29 21:21:15 +0330

    feat : add query 'ListProjectMembers' to queries in sqlc

--- internal/db/queries/project_member.sql
+++ internal/db/queries/project_member.sql
@@ -7,3 +7,10 @@ RETURNING *;
 -- name: GetProjectMemberRole :one 
 SELECT role FROM project_members 
 WHERE project_id = $1 AND user_id = $2;
+
+
+-- name: ListProjectMembers :many
+SELECT pm.* , u.full_name , u.email
+FROM project_members pm 
+JOIN user u ON u.id = pm.user_id
+WHERE project_id = $1;
