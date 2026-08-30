62bd62a9fbc250d1bca207a7df05a170730808bb
krsh <kiavash86habibi@gmail.com>
2026-08-30 16:00:20 +0330

    chore(handler) : add comment of teams table

---
+++ internal/team/handler.go
@@ -0,0 +1,11 @@
+package team
+
+import ()
+
+// teams (
+//     id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+//     name VARCHAR(255) NOT NULL,
+//     organization_id UUID NOT NULL REFERENCES organizations (id) ON DELETE CASCADE,
+//     created_at TIMESTAMPTZ NOT NULL DEFAULT now (),
+//     updated_at TIMESTAMPTZ NOT NULL DEFAULT now (),
+//     deleted_at TIMESTAMPTZ
