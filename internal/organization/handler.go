162a3bdaff513dcc879a31a9cfa0574ead583b8e
krsh <kiavash86habibi@gmail.com>
2026-08-30 12:57:19 +0330

    feat(handler) : get user id using param in RemoveMember

--- internal/organization/handler.go
+++ internal/organization/handler.go
@@ -201,4 +201,11 @@ func (h *Handler) RemoveMember(c *gin.Context) {
 		return
 	}
 
+
+	userID, err := uuid.Parse(c.Param("user_id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid type of user_id"})
+		return
+	}
+
 }
