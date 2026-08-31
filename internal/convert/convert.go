a13d52c1b3ed92258e637f6ee79d1ab3bb950047
krsh <kiavash86habibi@gmail.com>
2026-08-31 21:34:45 +0330

    chore(convert) : add func TimeToTimestamptz

--- internal/convert/convert.go
+++ internal/convert/convert.go
@@ -1,6 +1,10 @@
 package convert
 
-import "github.com/jackc/pgx/v5/pgtype"
+import (
+	"time"
+
+	"github.com/jackc/pgx/v5/pgtype"
+)
 
 func TextToString(t pgtype.Text) string {
 	if !t.Valid {
@@ -15,4 +19,12 @@ func StringToText(s string) pgtype.Text {
 		return pgtype.Text{}
 	}
 	return pgtype.Text{String: s, Valid: true}
+}
+
+func TimeToTimestamptz(t *time.Time) pgtype.Timestamptz {
+	if t == nil {
+		return pgtype.Timestamptz{}
+	}
+
+	return pgtype.Timestamptz{Time: *t, Valid: true}
 }
