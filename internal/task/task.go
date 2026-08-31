e9d558748a2dda64b5839987ad3bd665080671c3
krsh <kiavash86habibi@gmail.com>
2026-09-01 00:34:58 +0330

    chore(taskHandler) : Struct createTaskRequest

--- internal/task/task.go
+++ internal/task/task.go
@@ -1,6 +1,10 @@
 package task
 
-import "github.com/kiarash86/mitra/internal/db/sqlc"
+import (
+	"time"
+
+	"github.com/kiarash86/mitra/internal/db/sqlc"
+)
 
 type Handler struct {
 	queries *sqlc.Queries
@@ -10,4 +14,11 @@ func NewHandler(queries *sqlc.Queries) *Handler {
 	return &Handler{
 		queries: queries,
 	}
+}
+
+type createTaskRequest struct {
+	Title       string     `json:"title" binding:"required,min=2,max=255"`
+	Description string     `json:"description" binding:"max=10000"`
+	Priority    string     `json:"priority"`
+	DueDate     *time.Time `json:"due_date"`
 }
