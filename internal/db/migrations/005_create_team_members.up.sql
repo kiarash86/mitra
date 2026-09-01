dcc4334a35e2b19fb6e400d5b00edd2ff383cf55
krsh <kiavash86habibi@gmail.com>
2026-09-01 22:39:12 +0330

    chore(db) : delete team from migration and queries

--- internal/db/migrations/005_create_team_members.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE
-    team_members (
-        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-        team_id UUID NOT NULL REFERENCES teams (id) ON DELETE CASCADE,
-        user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-        role VARCHAR(50) NOT NULL DEFAULT 'member',
-        created_at TIMESTAMPTZ NOT NULL DEFAULT now (),
-        UNIQUE (team_id, user_id)
-    );
--- internal/db/migrations/010_drop_teams.up.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE tasks
-DROP COLUMN assigned_to_team_id;
-
-DROP TABLE team_members;
-
-DROP TABLE teams;
