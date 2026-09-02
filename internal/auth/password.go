76664f5e7f3edcbfae4260d76020bddd9776bd4a
krsh <kiavash86habibi@gmail.com>
2026-09-02 23:33:41 +0330

    feat(password) : func GenerateTempPassword using rand

--- internal/auth/password.go
+++ internal/auth/password.go
@@ -1,8 +1,15 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/rand"
+	"math/big"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 const bcryptCost = 12
+const tempPasswordLength = 12
+const tempPasswordAlphabet = "23456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ"
 
 func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcryptCost)
@@ -18,4 +25,20 @@ func CheckPassword(hash string, password string) bool {
 		return false
 	}
 	return true
+}
+
+func GenerateTempPassword() (string, error) {
+	password := make([]byte, tempPasswordLength)
+	for i := range password {
+		number, err := rand.Int(
+			rand.Reader,
+			big.NewInt(int64(len(tempPasswordAlphabet))),
+		)
+		if err != nil {
+			return "", err
+		}
+		password[i] = tempPasswordAlphabet[number.Int64()]
+	}
+
+	return string(password), nil
 }
