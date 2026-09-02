2d0398bc31e24c2c8635afd742ae76ab2269de87
krsh <kiavash86habibi@gmail.com>
2026-09-02 21:51:36 +0330

    chore(db) : add column to users table -> must_change_password

--- internal/db/migrations/002_create_users.up.sql
+++ internal/db/migrations/002_create_users.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE
         full_name VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL UNIQUE,
         password_hash VARCHAR(255) NOT NULL,
+        must_change_password BOOLEAN NOT NULL DEFAULT true,
         created_at TIMESTAMPTZ NOT NULL DEFAULT now (),
         updated_at TIMESTAMPTZ NOT NULL DEFAULT now (),
         deleted_at TIMESTAMPTZ
