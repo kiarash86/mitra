da86f68575c1f4afc3a392492ea2e4b713b95322
krsh <kiavash86habibi@gmail.com>
2026-09-03 16:17:48 +0330

    feat(seed): func getEnv -> get key return value

--- cmd/seed/main.go
+++ cmd/seed/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/kiarash86/mitra/internal/config"
 )
@@ -11,4 +12,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("couldnt load config: %v", err)
 	}
+}
+
+func getEnv(key string) string {
+	env := os.Getenv(key)
+	if env == "" {
+		log.Fatalf("missing env parameter: %s", key)
+	}
+
+	return env
 }
