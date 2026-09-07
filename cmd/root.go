3a89e3961b4dbf8717b3f38b29e062e43b015830
krsh <kiavash86habibi@gmail.com>
2026-09-07 12:38:51 +0330

    feat(cmd) : add rootCmd to root

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,22 @@
 package cmd
+
+import (
+	"log"
+
+	"github.com/kiarash86/mitra/internal/config"
+	"github.com/spf13/cobra"
+)
+
+var cfg *config.Config
+
+var rootCmd = &cobra.Command{
+	Use:   "mitra",
+	Short: "Mitra API server and operational tooling",
+	Run: func(cmd *cobra.Command, args []string) {
+		config, err := config.Load()
+		if err != nil {
+			log.Fatalf("couldnt load config: %v", err)
+		}
+		cfg = config
+	},
+}
