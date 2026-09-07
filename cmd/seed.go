fc9cabb7421ea172c5a8bca3d1f5224801036bf8
krsh <kiavash86habibi@gmail.com>
2026-09-07 13:00:22 +0330

    feat(cmd) :  add func init to set flags of cli and add seedCmd to rootCmd

--- cmd/seed.go
+++ cmd/seed.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-
 	"github.com/spf13/cobra"
 )
 
@@ -19,4 +18,22 @@ var seedCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		runSeed()
 	},
+}
+
+func firstNonEmpty(flagValue, envValue string) string {
+	if flagValue != "" {
+		return flagValue
+	}
+	return envValue
+}
+
+func init() {
+
+	seedCmd.Flags().StringVar(&seedOrgName, "org-name", "", "organization name (defaults to ORG_NAME)")
+	seedCmd.Flags().StringVar(&seedOrgSlug, "org-slug", "", "organization slug (defaults to ORG_SLUG)")
+	seedCmd.Flags().StringVar(&seedOwnerEmail, "owner-email", "", "owner login email (defaults to OWNER_EMAIL)")
+	seedCmd.Flags().StringVar(&seedOwnerName, "owner-name", "", "owner full name (defaults to OWNER_NAME)")
+	seedCmd.Flags().StringVar(&seedOwnerPassword, "owner-password", "", "owner initial password (defaults to OWNER_PASSWORD)")
+
+	rootCmd.AddCommand(seedCmd)
 }
