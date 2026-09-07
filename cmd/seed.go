b2bfcf940558a87f7bbb22a6ce94e25cedc9ae6c
krsh <kiavash86habibi@gmail.com>
2026-09-07 12:59:19 +0330

    feat(cmd) :  add seedCmd to seed

--- cmd/seed.go
+++ cmd/seed.go
@@ -1,5 +1,10 @@
 package cmd
 
+import (
+
+	"github.com/spf13/cobra"
+)
+
 var (
 	seedOrgName       string
 	seedOrgSlug       string
@@ -7,3 +12,11 @@ var (
 	seedOwnerName     string
 	seedOwnerPassword string
 )
+
+var seedCmd = &cobra.Command{
+	Use:   "seed",
+	Short: "Create the first organization and owner account",
+	Run: func(cmd *cobra.Command, args []string) {
+		runSeed()
+	},
+}
