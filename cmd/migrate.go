7d53b915f0432bc008a8a9fbd013120e5498366f
krsh <kiavash86habibi@gmail.com>
2026-09-07 12:40:48 +0330

    feat(cmd) : add migrateUpCmd to migrate path

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -1,10 +1,28 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/kiarash86/mitra/internal/db/migrator"
 	"github.com/spf13/cobra"
 )
 
 var migrateCmd = &cobra.Command{
 	Use:   "migrate",
 	Short: "Manage database migrations",
+}
+
+var migrateUpCmd = &cobra.Command{
+	Use:   "up",
+	Short: "Apply all available migrations",
+	Run: func(cmd *cobra.Command, args []string) {
+		err := migrator.Up(cfg.DatabaseURL)
+		if err != nil {
+			log.Fatalf("migrate up: %v", err)
+
+		}
+		fmt.Println("migrations applied successfully")
+
+	},
 }
