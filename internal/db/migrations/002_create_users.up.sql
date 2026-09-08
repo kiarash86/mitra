53a0da4dcacf3ce7edc21e795e71d815728d9ada
krsh <kiavash86habibi@gmail.com>
2026-09-08 12:44:30 +0330

    chore(db) : add role to user and delete organization from projects

--- internal/db/migrations/002_create_users.up.sql
+++ internal/db/migrations/002_create_users.up.sql
@@ -5,6 +5,7 @@ CREATE TABLE
         email VARCHAR(255) NOT NULL UNIQUE,
         password_hash VARCHAR(255) NOT NULL,
         must_change_password BOOLEAN NOT NULL DEFAULT true,
+        role VARCHAR(50) NOT NULL DEFAULT 'member',
         created_at TIMESTAMPTZ NOT NULL DEFAULT now (),
         updated_at TIMESTAMPTZ NOT NULL DEFAULT now (),
         deleted_at TIMESTAMPTZ
--- internal/db/migrations/004_create_projects.up.sql
+++ internal/db/migrations/004_create_projects.up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE
     projects (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-        organization_id UUID NOT NULL REFERENCES organizations (id) ON DELETE CASCADE,
         name VARCHAR(255) NOT NULL,
         description TEXT,
         created_at TIMESTAMPTZ NOT NULL DEFAULT now (),
