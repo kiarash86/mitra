34dfde0286314186caae724a2e23ec4846976955
krsh <kiavash86habibi@gmail.com>
2026-09-08 11:16:40 +0330

    perf(rootCmd) : instead of log.fatalf using fmt

--- cmd/root.go
+++ cmd/root.go
@@ -1,24 +1,29 @@
 package cmd
 
 import (
-	"log"
+	"fmt"
 	"os"
 
 	"github.com/kiarash86/mitra/internal/config"
 	"github.com/spf13/cobra"
 )
 
+var version = "dev"
+
 var cfg *config.Config
 
 var rootCmd = &cobra.Command{
-	Use:   "mitra",
-	Short: "Mitra API server and operational tooling",
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+	Use:          "mitra",
+	Short:        "Mitra API server and operational tooling",
+	Version:      version,
+	SilenceUsage: true,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 		config, err := config.Load()
 		if err != nil {
-			log.Fatalf("couldnt load config: %v", err)
+			return fmt.Errorf("couldnt load config: %w", err)
 		}
 		cfg = config
+		return nil
 	},
 }
 
