741b94d37830c473c80db9f302935bf05818c6b3
krsh <kiavash86habibi@gmail.com>
2026-09-08 11:32:39 +0330

    perf(rootCmd) : delete runE from rootCmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,35 +1,22 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/kiarash86/mitra/internal/config"
 	"github.com/spf13/cobra"
 )
 
 var version = "dev"
-
-var cfg *config.Config
 
 var rootCmd = &cobra.Command{
 	Use:          "mitra",
 	Short:        "Mitra API server and operational tooling",
 	Version:      version,
 	SilenceUsage: true,
-	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		config, err := config.Load()
-		if err != nil {
-			return fmt.Errorf("couldnt load config: %w", err)
-		}
-		cfg = config
-		return nil
-	},
 }
 
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
