e07e6487637b69fe101db4b2ae4c6a9c8c416a4b
krsh <kiavash86habibi@gmail.com>
2026-09-09 12:05:07 +0330

    chore(userHandler) : get userID using middleware in UpdateMe

--- internal/users/handler.go
+++ internal/users/handler.go
@@ -222,3 +222,12 @@ func (h *Handler) Me(c *gin.Context) {
 
 	c.JSON(http.StatusOK, toUserResponse(user))
 }
+
+func (h *Handler) UpdateMe(c *gin.Context) {
+	userID, ok := middleware.CurrentUserID(c)
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid Authorization"})
+		return
+	}
+
+}
